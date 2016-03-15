68acbe06aa4abaf366070561ab4162a0c793c040
Radu Almășan <radu.almasan@ness.com>
2016-03-15 11:04:34 +0200

    Use KeyValueTextInputFormat to read into XmlParserMapper

--- src/main/java/com/ness/bigdata/training/mapreduce/pubmed/XmlParserMapper.java
+++ src/main/java/com/ness/bigdata/training/mapreduce/pubmed/XmlParserMapper.java
@@ -1,8 +1,5 @@
 package com.ness.bigdata.training.mapreduce.pubmed;
 
-import java.io.IOException;
-import java.io.InputStream;
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
@@ -10,17 +7,16 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class XmlParserMapper extends Mapper<Object, Text, IntWritable, ArticleInfo> {
+import java.io.IOException;
+import java.io.InputStream;
+
+public class XmlParserMapper extends Mapper<Text, Text, IntWritable, ArticleInfo> {
 	private final static IntWritable ONE = new IntWritable(1);
 
-	@Override
-	protected void map(Object key, Text value, Mapper<Object, Text, IntWritable, ArticleInfo>.Context context)
-			throws IOException, InterruptedException {
-		String[] split = value.toString().split("\t");
-        String filePath = split[1];
-		
-		XmlParser fileParser = parseFile(new Path(filePath), context.getConfiguration());
-		ArticleInfo articleInfo = new ArticleInfo(filePath, fileParser.getTitle(), fileParser.getPublisherId(),
+    	@Override
+	protected void map(Text fileSize, Text filePath, Context context) throws IOException, InterruptedException {
+		XmlParser fileParser = parseFile(new Path(filePath.toString()), context.getConfiguration());
+		ArticleInfo articleInfo = new ArticleInfo(filePath.toString(), fileParser.getTitle(), fileParser.getPublisherId(),
 				fileParser.getIssnPPub(), fileParser.getAcceptedDate());
 		context.write(ONE, articleInfo);
 	}
--- src/test/java/com/ness/bigdata/training/mapreduce/pubmed/XmlParserJobTest.java
+++ src/test/java/com/ness/bigdata/training/mapreduce/pubmed/XmlParserJobTest.java
@@ -1,11 +1,5 @@
 package com.ness.bigdata.training.mapreduce.pubmed;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Calendar;
-
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.hadoop.io.AvroSerialization;
@@ -16,7 +10,6 @@ import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mrunit.mapreduce.MapReduceDriver;
 import org.junit.Before;
@@ -28,10 +21,16 @@ import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Calendar;
+
 @PrepareForTest(FileSystem.class)
 @RunWith(PowerMockRunner.class)
 public class XmlParserJobTest {
-    private MapReduceDriver<Object, Text, IntWritable, ArticleInfo, AvroKey<Integer>, AvroValue<GenericRecord>> mapReduceDriver;
+    private MapReduceDriver<Text, Text, IntWritable, ArticleInfo, AvroKey<Integer>, AvroValue<GenericRecord>> mapReduceDriver;
 
     @Mock
     private FileSystem fileSystemMock;
@@ -66,15 +65,17 @@ public class XmlParserJobTest {
 
     @Test
     public void testMapReduce() throws IOException {
+        Long fileSize = 1234L;
         String filePath = "/user/ubuntu/datasets/pubmed/unzipped/unzipped.A-B/3_Biotech/3_Biotech_2011_Dec_13_1(4)_217-225.nxml";
-        String line = "1\t" + filePath;
+
         Calendar calendar = Calendar.getInstance();
-        calendar.set(2011, 9 - 1, 28, 0, 0, 0);
+        calendar.set(2011, Calendar.SEPTEMBER, 28, 0, 0, 0);
         calendar.set(Calendar.MILLISECOND, 0);
-        ArticleInfo expectedArticleInfo = new ArticleInfo(filePath,
-                "Evaluation of indigenous Trichoderma isolates from Manipur as biocontrol agent against Pythium aphanidermatum on common beans",
-                27L, "2190-572X", calendar.getTimeInMillis());
-        mapReduceDriver.withInput(NullWritable.get(), new Text(line)).withOutput(new AvroKey<Integer>(1),
-                new AvroValue<GenericRecord>(expectedArticleInfo.toAvroGenericRecord())).runTest();
+        ArticleInfo expectedArticleInfo= new ArticleInfo(filePath, "Evaluation of indigenous Trichoderma isolates from Manipur as biocontrol agent against Pythium aphanidermatum on common beans", 27L, "2190-572X", calendar.getTimeInMillis());
+
+        mapReduceDriver
+                .withInput(new Text(fileSize.toString()), new Text(filePath))
+                .withOutput(new AvroKey<>(1), new AvroValue<>(expectedArticleInfo.toAvroGenericRecord()))
+                .runTest();
     }
 }
